2e35e26e973d94de6e4cf936daa4e1bee2ba474e
Chokun <chayanont.tan@spumail.net>
2025-08-29 22:38:43 +0700

    fix: CORS config for Firebase + localhost

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,12 @@ const catergoryRouter = require('../Routers/categoryRouter');
 const newRouter = require('../Routers/newRouter')
 
 
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: ["https://dadkaf-18b16ds.web.app", "http://localhost:5173"], // firebase + local dev
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api', recipeRouter);
